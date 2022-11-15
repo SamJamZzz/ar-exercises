0ac6d456d59363cac3d9277ff16620ddae267e02
Sam <samjulian6@gmail.com>
2022-11-14 17:43:24 -0800

    Finish exercise 5 by outputting various metrics for stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_annual_revenue = Store.all.sum("annual_revenue")
+puts "total annual revenue: $#{@total_annual_revenue}"
+
+@average_annual_revenue = Store.all.sum("annual_revenue") / Store.count("name")
+puts "average annual revenue: $#{@average_annual_revenue}"
+
+@num_stores = Store.where("annual_revenue >= ?", 1000000).count("name")
+puts "Number of stores that are generating $1M or more in annual sales: #{@num_stores}"
